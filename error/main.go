61b5878ba59e7bdf07c2d53d9bbc1fd5dd2ab8b9
Peng Xiao <xiaoquwl@gmail.com>
2016-08-16 23:48:37 +0800

    error process
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

---
+++ error/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	_, err := os.Open("no-file.txt")
+	if err != nil {
+		fmt.Println("error happened", err)
+		log.Fatalln(err)
+	}
+}
